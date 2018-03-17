c353251a73ba91b7916df0187f8170dfaebea473
VOUGY <guillaume.vouilloz@outlook.com>
2018-03-17 09:02:37 +0100

    Create class Log

---
+++ P2P_Server/src/Log.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.time.LocalDateTime;
+
+
+/**
+ * 
+ * @author guill
+ * @version 0.1
+ * Class who manage every transaction of the application
+ */
+
+
+public class Log {
+	/**
+	 * {@value} 1 : OK
+	 * 			2 : Warning
+	 * 			3 : Error
+	 */
+	int[] level = new int[3]; 
+	File file;
+	
+	
+	public Log ()
+	{
+		
+	}
+	
+	public void CreateLogFile()
+	{
+		
+	}
+	public void CreateLog(int level, String description)
+	{
+		System.out.println(LocalDateTime.now().toString());
+		// file.write ... (Date - Warning - Description
+	}
+}
